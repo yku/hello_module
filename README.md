923ff2026bc8049fcb224a0bd6d4772db1913c63
yku <kz2yz4@gmail.com>
2019-09-07 22:40:12 +0900

    Add sysfs(show)

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # hello_module
+
+- 下記のメッセージが出る場合は、MODULE_LICENSE("GPL")を追加する
+- 下記のシンボルはGPLライセンスのためドライバ側もGPLでないと見えない
+
+```
+[ 1560.239247] hello: Unknown symbol kernel_kobj (err 0)
+[ 1560.239307] hello: Unknown symbol sysfs_create_group (err 0)
+[ 1560.239356] hello: Unknown symbol kobject_create_and_add (err 0)
+```
+- kernel_kobjの子供として作成したため、/sys/kernel以下に配置される
+
+```
+# cat /sys/kernel/hello_mod/echo
+Hello World
+# cat /sys/kernel/hello_mod/version
+v0.1
+```
+
+- attribute_groupの.nameに値を設定するとその配下にパラメータが作成される
+
+```
+static struct attribute_group hello_attr_group = { 
+    .name = "hello_child",
+    .attrs = hello_sysfs_attrs,
+};
+
+
+# ls /sys/kernel/hello_mod/hello_child/
+echo  version
+```
--- hello.c
+++ hello.c
@@ -1,17 +1,65 @@
+#include <linux/kobject.h>
+#include <linux/device.h>
 #include <linux/module.h>
+#include <linux/sysfs.h>
+
+static struct kobject *g_kobj;
+
+static ssize_t
+show_version(struct device *dev, struct device_attribute *attr, char *buf)
+{
+    return sprintf(buf, "v0.1\n");
+}
+static DEVICE_ATTR(version, S_IRUGO, show_version, NULL);
+
+static ssize_t
+show_echo(struct device *dev, struct device_attribute *attr, char *buf)
+{
+    return sprintf(buf, "Hello World\n");
+}
+static DEVICE_ATTR(echo, S_IRUGO, show_echo, NULL);
+
+static struct attribute *hello_sysfs_attrs[] = {
+    &dev_attr_version.attr,
+    &dev_attr_echo.attr,
+    NULL
+};
+
+static struct attribute_group hello_attr_group = {
+    .name = "hello_child",
+    .attrs = hello_sysfs_attrs,
+};
+
+const struct attribute_group *hello_attr_groups[] = {
+    &hello_attr_group,
+    NULL
+};
 
 static int __init
 hello_init(void)
 {
+    int ret;
+    
     printk("%s called!\n", __func__);
-    return 0;
+
+    g_kobj = kobject_create_and_add("hello_mod", kernel_kobj);
+    if(!g_kobj)
+        return -ENOMEM;
+    ret = sysfs_create_groups(g_kobj, hello_attr_groups);
+    if(ret)
+        kobject_put(g_kobj);
+
+    return ret;
 }
 
 static void __exit
 hello_exit(void)
 {
     printk("%s called!\n", __func__);
+    if(g_kobj)
+        kobject_put(g_kobj);
 }
 
 module_init(hello_init);
 module_exit(hello_exit);
+MODULE_LICENSE("GPL");
